a9befe36367cc0fc56e7609c251abadb478d2db0
DeveloperTariq786 <shaheendeveloper12@gmail.com>
2026-04-20 14:58:25 +0530

    fix toast close issue and update images isue

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -9,6 +9,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
+      duration={1000}
       className="toaster group"
       toastOptions={{
         classNames: {
